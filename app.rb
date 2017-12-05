eb31dade0092b5c34811927fa4eb0dab3dadcfec
TJ Carney <tjcarney@stny.rr.com>
2017-12-05 20:07:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is TJ"
+  end
+
+  get '/hometown' do
+    "My hometown is Endwell, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Cute Without the E"
+  end
 end
